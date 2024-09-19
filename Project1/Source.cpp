aee20067920b0366ea7be11230a935bc32ea050c
hparadox82 <aust.i.mo34@gmail.com>
2024-09-18 20:58:23 -0400

    part2 prompt update

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -25,6 +25,12 @@ int main()
 	cout << oceanRise * 10 << "mm in ten years." << endl;
 
 			//PART 2
+			//A car with a 20-gallon gas tank averages 23.5 miles per gallon when driven in town, and 28.9
+			//miles per gallon when driven on the highway. Write a program that calculates and displays the
+			//distance the car can travel on one tank of gas when driven in town and when driven on the
+			//highway.
+			//Hint: The following formula can be used to calculate the distance:
+			//Distance = Number of Gallons x Average Miles per Gallon
 			double numbGal, avgMiGalTown, avgMiGalHigh;
 
 			numbGal = 20;
