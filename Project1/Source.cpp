20984ff3fcc1527042d4598876df498c7735d96a
hparadox82 <aust.i.mo34@gmail.com>
2024-09-18 20:11:27 -0400

    Add project files.

---
+++ Project1/Source.cpp
@@ -0,0 +1,7 @@
+//**Branches 1 and 2 were written on a separate work computer and emailed back to myself to input into VS2022
+//on my computer at home which explains a lack of history on github, since we're not allowed to install
+//VS or run code on work computers.
+
+//CH2 Programming Project CSC221 63H
+//Austin Mollard
+//Instructor Andrew Sprague
