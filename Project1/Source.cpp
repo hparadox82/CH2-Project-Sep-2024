073990582a3964296073f093877f0bff59deb029
hparadox82 <aust.i.mo34@gmail.com>
2024-09-18 20:23:50 -0400

    ran part1 successfully on master

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -5,3 +5,19 @@
 //CH2 Programming Project CSC221 63H
 //Austin Mollard
 //Instructor Andrew Sprague
+#include <iostream>
+#include <cmath>
+using namespace std;
+//Assuming the ocean’s level is currently rising at about 1.5 millimeters per year, write a program that displays:
+//The number of millimeters higher than the current level that the ocean’s level will be in 5yr
+//The number of millimeters higher than the current level that the ocean’s level will be in 7yr
+//The number of millimeters higher than the current level that the ocean’s level will be in 10yr
+int main()
+{
+	double oceanRise;
+	oceanRise = 1.5;
+	cout << "At a rate of 1.5mm per year, the ocean's sea level will rise " << oceanRise * 5 << "mm in five years, " << endl;
+	cout << oceanRise * 7 << "mm in seven years, and " << endl;
+	cout << oceanRise * 10 << "mm in ten years.";
+	return 0;
+}
