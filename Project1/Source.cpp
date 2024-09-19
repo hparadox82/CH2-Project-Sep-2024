86c2fa40b10009b1360a9d9f56d7d7ffb55afb24
hparadox82 <aust.i.mo34@gmail.com>
2024-09-18 20:26:10 -0400

    part2 pushed, runs/ver

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -8,16 +8,23 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-//Assuming the ocean’s level is currently rising at about 1.5 millimeters per year, write a program that displays:
-//The number of millimeters higher than the current level that the ocean’s level will be in 5yr
-//The number of millimeters higher than the current level that the ocean’s level will be in 7yr
-//The number of millimeters higher than the current level that the ocean’s level will be in 10yr
+//A car with a 20-gallon gas tank averages 23.5 miles per gallon when driven in town, and 28.9
+//miles per gallon when driven on the highway. Write a program that calculates and displays the
+//distance the car can travel on one tank of gas when driven in town and when driven on the
+//highway.
+//Hint: The following formula can be used to calculate the distance:
+//Distance = Number of Gallons x Average Miles per Gallon
+
 int main()
 {
-	double oceanRise;
-	oceanRise = 1.5;
-	cout << "At a rate of 1.5mm per year, the ocean's sea level will rise " << oceanRise * 5 << "mm in five years, " << endl;
-	cout << oceanRise * 7 << "mm in seven years, and " << endl;
-	cout << oceanRise * 10 << "mm in ten years.";
+	double numbGal, avgMiGalTown, avgMiGalHigh;
+
+	numbGal = 20;
+	avgMiGalTown = 23.5;
+	avgMiGalHigh = 28.9;
+
+	cout << "Distance driven at 23.5mpg in the city, on one tank of gas: " << numbGal * avgMiGalTown << endl;
+	cout << "Distance driven at 28.9mpg on the highway, on one tank of gas: " << numbGal * avgMiGalHigh << endl;
+
 	return 0;
 }
